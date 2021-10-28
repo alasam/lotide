a82e661bbeaaab6ac82f7000d66e41a7113eaf7c
Anthony <alasam@live.com>
2021-10-28 03:36:14 -0400

    keyFindByValue function a value in an object and returns the key it is in.

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  // if actual is equal to expected, then we will return a success message to the console
+  if (actual === expected) {
+    return console.log(`😃Assertion Passed😃: ${actual} === ${expected}`);
+    // if actual is not equal to expected, we will return a failed message to console
+  } else {
+    return console.log(`😡Assertion Failed😡:  ${actual}  !==  ${expected}`);
+  }
+};
+
+// function that fines a value in an object and returns the key it is in.
+const findKeyByValue = function(object, value) {
+  const arr = Object.keys(object); // assignes keys in object to an array
+  for (const item of arr) {
+    if (object[item] === value) { // if the value patches the value provided, the key is returned
+      return item;
+    }
+  }
+  return undefined;
+};
+
+// TEST
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
